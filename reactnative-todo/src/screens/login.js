a0447f16a3bb9e4f6c626d0d9a66f9f2cb5b3959
Ryan Bas <ryan.basmajian@forgerock.com>
2021-10-14 10:23:52 -0600

    task(Delete-unused-components): Removing unused and unneccessary files
    No longer have login/register specific directories due to the use of one
    form and the hook

--- reactnative-todo/src/screens/login.js
+++ reactnative-todo/src/screens/login.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Form } from '../components/common/form';
-import { NativeModules } from 'react-native';
-const { FRAuthBridge } = NativeModules;
 
 function Login() {
   return <Form action={{ type: 'login' }} />;
--- reactnative-todo/src/screens/register.js
+++ reactnative-todo/src/screens/register.js
@@ -1,42 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Form } from '../components/common/form';
-import { Loading } from '../components/utilities/loading';
-import { FRStep } from '@forgerock/javascript-sdk';
-import { NativeModules } from 'react-native';
-
-const { FRAuthBridge } = NativeModules;
 
 function Register() {
-  const [data, setStep] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function start() {
-      try {
-        await FRAuthBridge.logout();
-        const response = await FRAuthBridge.register();
-        const data = JSON.parse(response);
-        const step = new FRStep(data);
-
-        setStep(step);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    start();
-  }, []);
-
-  return loading ? (
-    <Loading message={'Checking your session'} />
-  ) : (
-    <Form
-      data={data}
-      action={{ type: 'register' }}
-      setStep={setStep}
-      setLoading={setLoading}
-    />
-  );
+  return <Form action={{ type: 'register' }} />;
 }
 
 export { Register };

098586fbcdcc26fb15aabc511496db0212dc7063
Justin Lowery <justin.lowery@forgerock.com>
2022-02-03 18:37:08 -0600

    feat(start): create start state for blog part 1

--- reactnative-todo/src/index.js
+++ reactnative-todo/src/index.js
@@ -8,41 +8,15 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import React, { useEffect } from 'react';
-import { NativeModules } from 'react-native';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-/*
- * Please ensure you have created an .env.js from the
- * .env.example.js template!
- */
-import { DEBUGGER_OFF } from '../.env';
 import Theme from './theme/index';
 import { AppContext, useGlobalStateMgmt } from './global-state';
 import Router from './router';
 
-const { FRAuthSampleBridge } = NativeModules;
-
 export default function App() {
   const stateMgmt = useGlobalStateMgmt({});
-
-  useEffect(() => {
-    async function start() {
-      /* *******************************************************************
-       * NATIVE BRIDGE SDK INTEGRATION
-       * Summary: Call Start method to initialize SDK
-       * -------------------------------------------------------------------
-       * Details: It's important to start the FR SDK at the root level!
-       * Start is needed to be called and resolved before any other Native
-       * methods can be used.
-       *
-       * Important: Use `start` as early on as possible!
-       ********************************************************************/
-      if (!DEBUGGER_OFF) debugger;
-      await FRAuthSampleBridge.start();
-    }
-    start();
-  }, []);
 
   return (
     <Theme>
--- reactnative-todo/src/router.js
+++ reactnative-todo/src/router.js
@@ -8,20 +8,14 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import React, { useContext, useEffect } from 'react';
-import { NativeModules } from 'react-native';
+import React, { useContext } from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { DEBUGGER_OFF } from '../.env';
 import Home from './screens/home';
 import Login from './screens/login';
 import Logout from './screens/logout';
-import Register from './screens/register';
-import Todos from './screens/todos';
-
-const { FRAuthSampleBridge } = NativeModules;
 
 const Tab = createBottomTabNavigator();
 const tabBarOptions = {
@@ -35,36 +29,7 @@ import { AppContext } from './global-state';
 
 export default function Navigation() {
   // Used for getting the global authentication state
-  const [{ authenticated }, { setAuthentication }] = useContext(AppContext);
-
-  useEffect(() => {
-    async function getUserInfo() {
-      try {
-        /** *********************************************************************
-         * NATIVE BRIDGE SDK INTEGRATION POINT
-         * Summary: Logout, end session and revoke tokens
-         * ----------------------------------------------------------------------
-         * Details: This is a bit overkill in most situations, but this is the most
-         * strict way of protecting a route. Other options are just checking
-         * global state (remove this useEffect entirely), or checking for
-         * stored tokens (instead, call `getAccessToken`).
-         ********************************************************************* */
-        if (!DEBUGGER_OFF) debugger;
-        await FRAuthSampleBridge.getUserInfo();
-      } catch (err) {
-        console.info('User has been logged out.');
-        /**
-         * If the above getUserInfo fails, then the user has been
-         * logged out, so clear our global state.
-         */
-        setAuthentication(false);
-      }
-    }
-    if (authenticated) {
-      getUserInfo();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authenticated]);
+  const [{ authenticated }] = useContext(AppContext);
 
   if (!authenticated) {
     return (
@@ -90,20 +55,6 @@ export default function Navigation() {
               ),
             }}
           />
-          <Tab.Screen
-            component={Register}
-            name="Sign Up"
-            options={{
-              tabBarLabel: 'Sign Up',
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="account-plus"
-                  color={color}
-                  size={size}
-                />
-              ),
-            }}
-          />
         </Tab.Navigator>
       </NavigationContainer>
     );
@@ -118,20 +69,6 @@ export default function Navigation() {
               tabBarLabel: 'Home',
               tabBarIcon: ({ color, size }) => (
                 <MaterialCommunityIcons name="home" color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            component={Todos}
-            name="Todos"
-            options={{
-              tabBarLabel: 'Todos',
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="check-all"
-                  color={color}
-                  size={size}
-                />
               ),
             }}
           />
